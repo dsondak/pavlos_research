a05d080be623a48f7da8647f60437bd575439db0
pblankley <31221512+pblankley@users.noreply.github.com>
2018-03-20 11:41:27 -0400

    add transfer option to reinforcement

--- src/reinforcement.py
+++ src/reinforcement.py
@@ -11,9 +11,10 @@ from tqdm import tqdm
 
 # TODO docs for all 
 class Environment(object):
-    def __init__(self, model, train_x, train_y, val_x, val_y, loss_func, optimizer, params='default'):
+    def __init__(self, model, train_x, train_y, val_x, val_y, loss_func, optimizer, usps_data=None, params='default'):
         self.use_cuda = torch.cuda.is_available()
         self.model = model.cuda() if self.use_cuda else model
+        self.usps_train_loader = usps_data
         self.train_x = train_x
         self.train_y = train_y
         self.val_x = val_x
@@ -40,6 +41,29 @@ class Environment(object):
         if 'shuffle' in keys:
             self.shuffle = kwargs['shuffle']
 
+    def train_usps(self, epochs, model, opt):
+        losses,n_itr = [],0
+        for e in tqdm(range(epochs)):
+            for batch_x, batch_y in self.usps_train_loader:
+                batch_x = Variable(batch_x.cuda()) if use_cuda else Variable(batch_x)
+                batch_y = Variable(batch_y.cuda()) if use_cuda else Variable(batch_y)
+
+                result = model(batch_x)
+                loss = self.loss_func(result, batch_y)
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+
+                n_itr+=1
+                losses.append(loss)
+
+        if use_cuda:
+            losses = [itm.data.cpu().numpy()[0] for itm in losses]
+        else:
+            losses = [itm.data.numpy()[0] for itm in losses]
+
+        return list(range(n_itr)), losses
+
     # The incoming state is the marginals on all the points
     def select_action(self, agent, state):
         """ Get the probabilities for different actions and sample to select one. """
@@ -47,7 +71,7 @@ class Environment(object):
         m = Categorical(probs)
         action = m.sample()
         agent.saved_log_probs.append(m.log_prob(action))
-        return action.data[0]
+        return n.data[0]
 
     def finish_experiment(self, agent, optimizer, gamma=0.99):
         """ after the training episode is done, we can finally backprop over the expectation of the rewards. """
@@ -76,21 +100,27 @@ class Environment(object):
         rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
         return rewards
 
-    def reset_al(self, lr):
+    def reset_env(self, lr, reset_type='active', usps_init_epochs=1):
         ### Reset the envorinment
-        model_try = type(self.model)()
-        optimizer_try = type(self.optimizer)(model_try.parameters(), lr=lr)
+        if reset_type=='active':
+            model_try = type(self.model)()
+            optimizer_try = type(self.optimizer)(model_try.parameters(), lr=lr)
+        elif reset_type=='transfer':
+            model_try = type(self.model)()
+            optimizer_try = type(self.optimizer)(model_try.parameters(), lr=lr)
+            _, _ = self.train_usps(usps_init_epochs, model_try, optimizer_try)
+
         experiment = al.ExperiAL(model_try, self.train_x, self.train_y, self.val_x, self.val_y, self.loss_func, optimizer_try)
         # Note the meta epochs below (in AL) refers to the number of times we iterate a AL policy, only 1 obviously.
         experiment.set_params(meta_epochs=1, npoints=self.npoints, batch_size=self.batch_size, epochs_per_train=self.ept)
-        return experiment, model_try
+        return experiment, model_try, optimizer_try
 
-    def run_experiments(self, agent, optimizer_agent, policy_key, n_experiments, lr=0.01):
+    def run_experiments(self, agent, optimizer_agent, policy_key, n_experiments, rtype='active', lr=0.01):
         running_reward = 1.0
         policies_chosen, rewards_total = [],[]
         for i_exp in tqdm(range(n_experiments)):
             ### Reset the envorinment
-            experiment, model_try = self.reset_al(lr=lr)
+            experiment, model_try, optimizer_try = self.reset_env(lr=lr, reset_type=rtype, usps_init_epochs=1)
 
             # Calculate the initial state
             train_tensor = self.train_x.cuda() if self.use_cuda else self.train_x
@@ -100,8 +130,13 @@ class Environment(object):
             policies,track_reward = [],[]
             for t in range(self.al_itrs):
                 action = self.select_action(agent, state)
+                if policy_key[action]=='transfer':
+                    _,_ = self.train_usps(self.ept, model_try, optimizer_try)
+                    _,_ = experiment._train(self.lab_x, self.lab_y)
+                    reward = al.accuracy(model_try, self.val_x, self.val_y)
+                elif policy_key[action]=='active':
+                    _, reward = experiment.active_learn(policy=policy_key[action])
                 policies.append(policy_key[action])
-                _, reward = experiment.active_learn(policy=policy_key[action])
                 track_reward.append(reward)
                 state = model_try(Variable(train_tensor))
                 agent.rewards.append(reward[0])
